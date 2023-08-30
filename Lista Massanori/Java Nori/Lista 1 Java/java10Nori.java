5ae0d47d4e37a655f204b25c7905639e5286f460
Renaltas <pedromoucamm@gmail.com>
2023-08-30 07:44:13 -0300

    Correção java10Nori

--- Lista Massanori/Java Nori/Lista 1 Java/java10Nori.java
+++ Lista Massanori/Java Nori/Lista 1 Java/java10Nori.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class java10Nori {
     public static void main (String args[]){
-        System.out.println("Quantos cigarros você fuma por dia?");
         Scanner ler = new Scanner(System.in);
-        double cigF,anosfumados;
-        cigF = ler.nextDouble();
         
-        cigF *= 144;
-        System.out.println("o numero de dias perdidos é: " + cigF);
-
+        System.out.println("Quantos cigarros você fuma por dia?");
+        double cigarrosPorDia = ler.nextDouble();
+        
+        System.out.println("Há quantos anos você fuma?");
+        double anosFumados = ler.nextDouble();
+        
+        double cigarrosTotais = cigarrosPorDia * 365 * anosFumados; // Total de cigarros fumados
+        
+        // Cada cigarro reduz 10 minutos de vida
+        double minutosPerdidos = cigarrosTotais * 10;
+        
+        // Convertendo minutos para dias (1 dia = 1440 minutos)
+        double diasPerdidos = minutosPerdidos / 1440;
+        
+        System.out.println("Dias de vida perdidos devido ao hábito de fumar: " + diasPerdidos);
+        
         ler.close();
 
         
